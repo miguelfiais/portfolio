71acd0c17ea4401d4887eaeffc84a54222922aa6
miguelfiais <miguel.fiais10@gmail.com>
2023-01-17 09:36:53 -0300

    correção do texto about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,9 +10,9 @@ const About = () => {
        <BoxInfo>
           <img src={me} alt="foto-de-perfil" />
           <div>
-            <p>Meu nome é Miguel Fiais, tenho 21 anos, moro em Cruz das Almas-BA.</p>
+            <p>Meu nome é Miguel Fiais, tenho 21 anos e sou de Cruz das Almas-BA.</p>
             <br />
-            <p>Sou um desenvolvedor Front-End focado no desenvolvimento Web. Gosto de solucionar problemas, criar novas aplicações através dos códigos e lidar com inovações na área de tecnologia. Estou constantemente aprimorando meus conhecimentos e também aprendendo novas tecnologias que fazem parte Back-End para eu ser um profissional Full-Stack.</p>
+            <p>Sou um desenvolvedor Front-End focado em aplicações Web. Gosto muito de transformar minhas ideias em códigos e ver minhas aplicações se tornarem realidade. Estou sempre em busca de aprender novas habilidades e técnicas para me tornar um profissional ainda melhor e estar sempre atualizado com as tendências tecnológicas.</p>
           </div>
         </BoxInfo>
         <Title title="CONHECIMENTOS"/>
